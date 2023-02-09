48914db82888c766ba5aeb119f6f8f0b2c8b76b5
Jovanney <jjsl@cin.ufpe.br>
2023-02-09 11:01:22 -0300

    fase media

---
+++ blog/main.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI, Depends, status, Response, HTTPException
+from . import schemas, models
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+
+app = FastAPI()
+
+models.Base.metadata.create_all(engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title= request.title, body=request.body)
+
+    db.add(new_blog)
+
+    db.commit()
+
+    db.refresh(new_blog)
+
+    return new_blog
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")     
+    blog.delete(synchronize_session=False)
+    db.commit()   #always update de db after a operation
+    return 'Done'
+     
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if blog.first():  
+        blog.update(request)
+        db.commit()
+        return 'changed'
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog not found")
+
+
+
+@app.get('/blog')
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blog/{id}', status_code=200)
+def show(id, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with the {id} doesn't exist") 
+    return blog
--- main.py
+++ main.py
@@ -1,12 +1,48 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get('/')
-
 def index():
     return {'data':{'name': 'Jova'}}
 
-@app.get('/about')
-def about():
-    return "textAbout"
+@app.get('/blog')
+def about(limit: int =10, published: bool = True, sort: Optional[str] = None): #set 10 and True by default
+    #only get Limit published blogs
+    
+    if published:
+        return {'data': f'{limit} blogs from the db'}
+    else:
+        return {'data': f'there are not any published blogs'}
+    
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def index(id: int):
+    #fetch blog with id = id
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    #fetch comments of blog with id = id
+    return {'data': {'1', '2'}}
+
+#Post = Create something
+
+class Blog(BaseModel):  #Model
+    title: str
+    body: str
+    published_at: Optional[bool]
+    
+
+@app.post('/blog')
+def createBlog(request: Blog):
+    return {'data': f"Blog is created with title as {request.title}"}
+
+#SQLAlchemy's philosophy
+
